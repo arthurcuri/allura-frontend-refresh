fd6aa4d59cd142458a402c34f3d421fd0ddd40b8
Arthur Kramberger <arthur1curi@gmail.com>
2024-09-04 12:53:24 -0300

    Criando a base de dados com JavaScript

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Ciclista olímpica, ela se tornou um ícone da juventude e inspira milhares de pessoas.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com uma carreira promissora, ela já conquistou diversas medalhas em competições nacionais e internacionais.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta artística brasileira que fez história nos Jogos Olímpicos de Tóquio 2020. Com sua força e elegância, ela se tornou um dos maiores nomes da ginástica mundial.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    }
+];
