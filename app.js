1bd8235bb878104e3c979dffe896baf5b3df42ef
Arthur Kramberger <arthur1curi@gmail.com>
2024-09-06 18:32:57 -0300

    Finalizando o projeto com GitHub e Deploy

--- app.js
+++ app.js
@@ -1,25 +1,57 @@
-// Função para pesquisar e exibir os resultados na seção "resultados-pesquisa"
 function pesquisar() {
-  // Obtém a seção onde os resultados serão exibidos
+  // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
   let section = document.getElementById("resultados-pesquisa");
 
-  // Inicializa uma string vazia para armazenar os resultados
-  let resultados = "";
+  // Obtém o valor digitado no campo de pesquisa
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-  // Itera sobre cada dado no array de dados
-  for (let dado of dados) {
-    // Constrói o HTML para cada item do resultado
-    resultados += `
-      <div class="item-resultado">
-        <h2>
-          <a href="#" target="_blank">${dado.titulo}</a>
-        </h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href="${dado.link}" target="_blank">Mais informações</a>
-      </div>
-    `;
+  // Verifica se o campo de pesquisa está vazio
+  if (!campoPesquisa) {
+    // Exibe uma mensagem informando que o campo de pesquisa está vazio
+    section.innerHTML = "<p>Por favor, digite algo para pesquisar</p>";
+    return;
   }
 
-  // Atribui o HTML construído à seção de resultados
+  // Converte o valor do campo de pesquisa para minúsculas
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  // Inicializa uma string vazia para armazenar os resultados da pesquisa
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+  // Itera sobre os dados disponíveis (presumivelmente uma lista de objetos)
+  for (let dado of dados) {
+    // Converte os campos de título, descrição e tags para minúsculas
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+
+    // Verifica se o termo de pesquisa está contido no título, descrição ou tags
+    if (
+      titulo.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa) ||
+      tags.includes(campoPesquisa)
+    ) {
+      // Se houver uma correspondência, adiciona um novo elemento de resultado formatado
+      resultados += `
+          <div class="item-resultado">
+              <h2>
+                  <a href="#" target="_blank">${dado.titulo}</a>
+              </h2>
+              <p class="descricao-meta">${dado.descricao}</p>
+              <a href=${dado.link} target="_blank">Mais informações</a>
+          </div>
+      `;
+    }
+  }
+
+  // Se nenhum resultado foi encontrado, exibe uma mensagem apropriada
+  if (!resultados) {
+    resultados = "<p>Nada foi encontrado</p>";
+  }
+
+  // Insere os resultados (ou mensagem de nada encontrado) na seção HTML
   section.innerHTML = resultados;
 }
--- dados.js
+++ dados.js
@@ -4,6 +4,7 @@ let dados = [
     descricao:
       "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Ciclista olímpica, ela se tornou um ícone da juventude e inspira milhares de pessoas.",
     link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8",
   },
 
   {
@@ -11,6 +12,7 @@ let dados = [
     descricao:
       "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com uma carreira promissora, ela já conquistou diversas medalhas em competições nacionais e internacionais.",
     link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoka judoca",
   },
 
   {
@@ -18,5 +20,6 @@ let dados = [
     descricao:
       "Rebeca Andrade é uma ginasta artística brasileira que fez história nos Jogos Olímpicos de Tóquio 2020. Com sua força e elegância, ela se tornou um dos maiores nomes da ginástica mundial.",
     link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginatica ginasta ginástica ouro",
   },
 ];
